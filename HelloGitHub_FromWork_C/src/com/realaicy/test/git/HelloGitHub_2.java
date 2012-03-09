35cdb45d7457545f356312ca63ea20b653d811b7
realaicy <realaicy@126.com>
2012-03-09 11:16:16 +0800

    Update HelloGitHub_FromWork_C/src/com/realaicy/test/git/HelloGitHub_2.java

--- HelloGitHub_FromWork_C/src/com/realaicy/test/git/HelloGitHub_2.java
+++ HelloGitHub_FromWork_C/src/com/realaicy/test/git/HelloGitHub_2.java
@@ -17,6 +17,7 @@ public class HelloGitHub_2 {
 		System.out.println("Hello Github222222 I am Xudong Liu 刘旭东________________EclipseINIT");
 		System.out.println("Edit By GitHub Github222222 I am Xudong Liu 刘旭东________________GitHub");
 		System.out.println("Edit By GitHub 333333333333 I am Xudong Liu 刘旭东________________GitHub");
+		System.out.println("Edit By GitHub 444444444444 I am Xudong Liu 刘旭东________________GitHub");
 
 	}
 
