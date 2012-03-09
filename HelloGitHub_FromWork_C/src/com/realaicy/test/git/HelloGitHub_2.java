ea4e13189b03e75999e585c2fd5116b1a3c8a6c7
realaicy <realaicy@126.com>
2012-03-09 10:54:33 +0800

    commit by eclipse init

---
+++ HelloGitHub_FromWork_C/src/com/realaicy/test/git/HelloGitHub_2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.realaicy.test.git;
+
+/**
+ * @author REALAICY
+ *
+ */
+public class HelloGitHub_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello Github222222 I am Xudong Liu 刘旭东________________EclipseINIT");
+
+	}
+
+}
